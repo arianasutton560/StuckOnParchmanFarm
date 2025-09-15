4e9d0fc17e3d00a3f0431f9860eaf523004f3f97
Ariana Sutton <agsutton@smu.edu>
2025-09-15 18:19:09 -0500

    Add Player Interaction Script

--- Assets/TutorialInfo/Scripts/PlayerInteraction.cs
+++ Assets/TutorialInfo/Scripts/PlayerInteraction.cs
@@ -1,7 +1,8 @@
 using UnityEngine;
 
-//This Script allows the player to control their character
-public class PlayerController : MonoBehaviour
+//This Script allows the player to interact with objects in the game world
+//(e.g., picking up items, opening doors, Press E to interact)
+public class PlayerInteractable : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
