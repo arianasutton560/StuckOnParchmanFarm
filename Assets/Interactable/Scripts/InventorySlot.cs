916a5dfbcd6b07b82c2a6354490c8113cfd6646a
Ariana Sutton <agsutton@smu.edu>
2025-10-30 09:37:21 -0500

    Prototype 6 Final Commit

--- Assets/Interactable/Scripts/InventorySlot.cs
+++ Assets/Interactable/Scripts/InventorySlot.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 namespace EJETAGame.Inventory
 {
     public class InventorySlot : MonoBehaviour
     {
         public Image icon;
+        public TMP_Text nametext;
         private Item item;
 
         public void SetItem(Item newItem)
@@ -14,13 +16,19 @@ namespace EJETAGame.Inventory
             if (icon != null && item.icon != null)
             {
                 icon.sprite = item.icon;
-                icon.color = Color.white; 
+                icon.color = Color.white;
                 Debug.Log($"✅ Assigned icon for {item.itemName}: {item.icon.name}");
             }
             else
             {
                 Debug.LogWarning($"⚠️ Missing icon or item icon for {newItem.itemName}");
             }
+            
+            if( nametext != null)
+            {
+                nametext.text = item.getItemName();
+            }
+            
         }
 
     }
