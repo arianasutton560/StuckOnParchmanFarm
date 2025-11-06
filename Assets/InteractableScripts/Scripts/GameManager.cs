40dd2ccabcc82ccb7e07a7de3a623fe1e97a73f0
Ariana Sutton <agsutton@smu.edu>
2025-11-06 16:21:38 -0600

    edit GameManager

--- Assets/InteractableScripts/Scripts/GameManager.cs
+++ Assets/InteractableScripts/Scripts/GameManager.cs
@@ -6,6 +6,10 @@ public class GameManager : MonoBehaviour
     public void RestartGame()
     {
         Time.timeScale = 1f;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
